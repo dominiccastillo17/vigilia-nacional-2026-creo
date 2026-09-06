20ec377286b911fdb9654419afcc1fd4984ae3d1
Dominic Oconor Castillo <dominiccastillo1020@gmail.com>
2026-09-06 11:11:10 -0600

    Update superbase-config.js

--- superbase-config.js
+++ superbase-config.js
@@ -1,5 +1,8 @@
-NEXT_PUBLIC_SUPABASE_URL=https://kkbdsfqquhbywgzwgfkq.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_M8303JtfWblTG9mxEvYYuw_tncTq2bE
+const SUPABASE_URL = 
+  "https://kkbdsfqquhbywgzwgfkq.supabase.co";
+const SUPABASE_ANON_KEY = 
+  "sb_publishable_M8303JtfWblTG9mxEvYYuw_tncTq2bE";
+
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
