dee733dcf58eccfc47c032c7d2e732d4b29f669c
Dominic Oconor Castillo <dominiccastillo1020@gmail.com>
2026-09-06 11:25:23 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,7 @@
-const SUPABASE_URL = "https://kkbdsfqquhbywgzwgfkq.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_M8303JtfWblTG9mxEvYYuw_tncTq2bE";
+const SUPABASE_URL = 
+  "https://kkbdsfqquhbywgzwgfkq.supabase.co";
+const SUPABASE_ANON_KEY = 
+  "sb_publishable_M8303JtfWblTG9mxEvYYuw_tncTq2bE";
 
 window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
