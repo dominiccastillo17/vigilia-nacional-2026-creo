7e3d7590714f9c999bb39b819664c4cd4de5404c
Dominic Oconor Castillo <dominiccastillo1020@gmail.com>
2026-09-06 13:18:53 -0600

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://kkbdsfqquhbywgzwgfkq.supabase.co";
-
-const SUPABASE_ANON_KEY =
-  "sb_publishable_M8303JtfWblTG9mxEvYYuw_tncTq2bE";
-
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
